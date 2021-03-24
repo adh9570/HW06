b3d24bf9ba76b4066fb908b2c75a31f70177b01d
Alexis Halbur <adh9570@rit.edu>
2021-03-24 14:56:39 -0400

    fixed up and consolodated cross corr method

--- HW_06.py
+++ HW_06.py
@@ -4,24 +4,22 @@ import statistics
 
 
 ### Derived from slide 44 of Distance Metrics part A slides
-### TODO: currently this is a generic implementation of the algorithm, will 
-### need to be fleshed out more when we figure out exactly what S and T are
-def computeCrossCorrelation(s, t):
-    # p(s, t) = 1/N * sum(from 1->N)[((sk - us)/os) * ((tk - ut)/ot)]
-    # where N is ??? the number of elements in the set ??
-    # where us is the average of S and ut is the average of T
-    # where os is the standard deviation of S and same for ot and T
-    N = len(s)                  # TODO: THIS IS A GUESS FIX IT ALL
+### Takes in two arrays, attributes S and T, and uses the average of all of
+### the values in each attribute, along with the standard deviation of the 
+### values to calculate the cross correlation coefficient between the two 
+### attributes
+def computeCrossCorrelation(attributeS, attributeT):
+    N = len(attributeS)
 
-    avgS = np.sum(s) / len(s)
-    avgT = np.sum(t) / len(t)
+    avgS = np.sum(attributeS) / len(attributeS)
+    avgT = np.sum(attributeT) / len(attributeT)
 
-    stdDevS = statistics.stdev(s)
-    stdDevT = statistics.stdev(t)
+    stdDevS = statistics.stdev(attributeS)
+    stdDevT = statistics.stdev(attributeT)
 
     crossCorrSum = 0
     for k in range(0, N):
-        crossCorrSum += ((s[k] - avgS)/stdDevS) * ((t[k] - avgT)/stdDevT)
+        crossCorrSum += ((attributeS[k] - avgS)/stdDevS) * ((attributeT[k] - avgT)/stdDevT)
 
     return crossCorrSum/N
 
@@ -29,15 +27,16 @@ def computeCrossCorrelation(s, t):
 ### Go through every row in the data and compare it to every other row of data,
 ### compute the cross correlation, and put that value into an NxN matrix of all
 ### of the cross correlation values
-def getCrossCorrMatrix(data):
+def getCrossCorrMatrix(dataframe):
+    dataWithoutID = dataframe.iloc[:,1:]                    # get the dataframe without the ID column
+    data = dataWithoutID.values                             # turn dataframe into 2D array for use in cross correlation computations
+
     matrix = np.empty((len(data), len(data)))               # create an empty matrix that will hold our cross correlation values
     for rowIndexX in range(0, len(data)):                   # go through each row of the data (called X row)
-        # dataRowX = data.iloc[rowIndexX][1:]                 # get the X row exclusing the ID
         dataRowX = data[rowIndexX]
         for rowIndexY in range(0, len(data)):               # compare to every other row of the data (called Y row)
             if rowIndexX == rowIndexY:                      # skip comparing a row to itself
                 continue
-            # dataRowY = data.iloc[rowIndexY][1:]             # get the Y row exclusing the ID
             dataRowY = data[rowIndexY]
             matrix[rowIndexX][rowIndexY] = computeCrossCorrelation(dataRowX, dataRowY)  # add cross correlation to the matrix at position [X, Y]
     print(matrix)
@@ -47,9 +46,7 @@ def getCrossCorrMatrix(data):
 def main():
     # data = pd.read_csv("HW_PCA_SHOPPING_CART_v896.csv")
     data = pd.read_csv("sample data.csv")
-    dataWithoutID = data.iloc[:,1:]                         # get the dataframe without the ID column
-    dataArray = dataWithoutID.values                        # turn dataframe into 2D array for use in cross correlation computations
-    getCrossCorrMatrix(dataArray)
+    getCrossCorrMatrix(data)
     
 
 if __name__ == '__main__':
