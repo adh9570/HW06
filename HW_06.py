570ef36061f0cef2384f3f18df1bb3fd38ddc769
Alexis Halbur <adh9570@rit.edu>
2021-03-28 15:16:22 -0400

    made graph more readable, print cluster sizes

--- HW_06.py
+++ HW_06.py
@@ -3,7 +3,8 @@ import numpy as np
 import statistics
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
 
 def kmeansCluster(values):
     kmeans = KMeans(n_clusters=6)
@@ -17,16 +18,36 @@ def kmeansCluster(values):
             centersFile.write(str(j) + ', ')
         centersFile.write('\n')
 
-    plt.scatter(values[kmv == 0, 0], values[kmv == 0, 1], s=50, c='r', marker='o', edgecolor='black', label='cluster1', alpha=0.25)
-    plt.scatter(values[kmv == 1, 0], values[kmv == 1, 1], s=50, c='g', marker='o', edgecolor='black', label='cluster2', alpha=0.25)
-    plt.scatter(values[kmv == 2, 0], values[kmv == 2, 1], s=50, c='b', marker='o', edgecolor='black', label='cluster3', alpha=0.25)
-    plt.scatter(values[kmv == 3, 0], values[kmv == 3, 1], s=50, c='c', marker='o', edgecolor='black', label='cluster4', alpha=0.25)
-    plt.scatter(values[kmv == 4, 0], values[kmv == 4, 1], s=50, c='m', marker='o', edgecolor='black', label='cluster5', alpha=0.25)
-    plt.scatter(values[kmv == 5, 0], values[kmv == 5, 1], s=50, c='y', marker='o', edgecolor='black', label='cluster6', alpha=0.25)
+    clusterSizes = [0] * 6
+    for value in kmv:
+        if value == 0:
+            clusterSizes[0] += 1
+        elif value == 1:
+            clusterSizes[1] += 1
+        elif value == 2:
+            clusterSizes[2] += 1
+        elif value == 3:
+            clusterSizes[3] += 1
+        elif value == 4:
+            clusterSizes[4] += 1
+        elif value == 5:
+            clusterSizes[5] += 1
+        elif value == 6:
+            clusterSizes[6] += 1
+        
+    print("Cluster Sizes", clusterSizes)
+
+    plt.scatter(values[kmv == 0, 0], values[kmv == 0, 1], s=50, c='r', marker='o', label='cluster1', alpha=0.25)
+    plt.scatter(values[kmv == 1, 0], values[kmv == 1, 1], s=50, c='g', marker='o', label='cluster2', alpha=0.25)
+    plt.scatter(values[kmv == 2, 0], values[kmv == 2, 1], s=50, c='b', marker='o', label='cluster3', alpha=0.25)
+    plt.scatter(values[kmv == 3, 0], values[kmv == 3, 1], s=50, c='c', marker='o', label='cluster4', alpha=0.25)
+    plt.scatter(values[kmv == 4, 0], values[kmv == 4, 1], s=50, c='m', marker='o', label='cluster5', alpha=0.25)
+    plt.scatter(values[kmv == 5, 0], values[kmv == 5, 1], s=50, c='y', marker='o', label='cluster6', alpha=0.25)
 
     plt.legend(scatterpoints=1)
     plt.grid()
     plt.show()
+
 
 
 ### Derived from slide 44 of Distance Metrics part A slides
@@ -76,7 +97,7 @@ def main():
     dataWithoutID = data.iloc[:,1:]
     columnsCrossCorr(dataWithoutID)
     values = dataWithoutID.values
-    kmeansCluster(values)    
+    kmeansCluster(values)
 
 
 if __name__ == '__main__':
